2855add3661e2345375570c5afd23b4ba0136042
Mohd Maruf <mmaruf@banquemisr.com>
2022-11-10 00:26:58 +0530

    suppor files added

---
+++ MarufTestFramework/MarufTestFramework/ColorConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ColorConstants.swift
+//  MarufTestFramework
+//
+//  Created by Maruf on 10/11/22.
+//
+
+import Foundation
+
+class ColorConstantsFile {
+    
+    static func getColorName(_ number: Int) -> String {
+        return "\(number)"
+    }
+}
