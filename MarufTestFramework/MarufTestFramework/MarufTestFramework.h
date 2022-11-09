d2004f6c99e12677ecb1becd7224401d983184c2
Mohd Maruf <mmaruf@banquemisr.com>
2022-11-10 00:20:38 +0530

    proj added

---
+++ MarufTestFramework/MarufTestFramework/MarufTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  MarufTestFramework.h
+//  MarufTestFramework
+//
+//  Created by Maruf on 10/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarufTestFramework.
+FOUNDATION_EXPORT double MarufTestFrameworkVersionNumber;
+
+//! Project version string for MarufTestFramework.
+FOUNDATION_EXPORT const unsigned char MarufTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarufTestFramework/PublicHeader.h>
+
+
